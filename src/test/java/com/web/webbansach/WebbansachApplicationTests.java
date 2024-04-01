7975b8fbe43dbddc6c5ea5f7932fc0ab8857ba01
tuilaphuc <anphuc0412@gmail.com>
2024-04-02 01:17:35 +0700

    first commit

---
+++ src/test/java/com/web/webbansach/WebbansachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.webbansach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
